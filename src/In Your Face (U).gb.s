2686cb6005b7121f4f0b848736fbbd6a3ade9e60
mattcurrie <me@mattcurrie.com>
2017-12-21 14:53:50 +1300

    Generate patches using patch-builder.py

---
+++ src/In Your Face (U).gb.s
@@ -0,0 +1,66 @@
+; md5 a4fc8158d1f2b6359ddac6c66d747a95
+
+.INCLUDE "includes/init.s"
+.ROMBANKS 8
+.BACKGROUND "In Your Face (U).gb"
+.INCLUDE "includes/header.s"
+
+
+;**********
+;* config *
+;**********
+
+.DEFINE current_rom_bank $ffe6
+
+
+;*************
+;* reset ram *
+;*************
+
+.DEFINE RESET_RAM_DONE $0150
+.BANK $0000 SLOT 0
+
+.ORG $00a3
+.SECTION "reset ram" SIZE $F OVERWRITE
+    .INCLUDE "includes/reset_ram.s"
+.ENDS
+
+.ORG $0101
+.SECTION "reset ram jump" SIZE 3 OVERWRITE
+    jp RESET_RAM
+.ENDS
+
+
+;**********
+;* joypad *
+;**********
+
+.DEFINE joypad $ffd5
+.DEFINE joypad_2 $ffd6
+
+.BANK $0000 SLOT 0
+.ORG $0062
+.SECTION "relocated read from joypad" SIZE $40 OVERWRITE
+    .INCLUDE "includes/relocated_read_from_joypad.s"
+.ENDS
+
+.ORG $028c
+.SECTION "joypad read" SIZE 4 OVERWRITE
+    call relocated_read_from_joypad
+    nop
+.ENDS
+
+
+;*******************
+;* save/load state *
+;*******************
+
+.BANK $0000 SLOT 0
+.ORG $3839
+.SECTION "save/load state" SIZE $0220 OVERWRITE
+    .DB "--- Save Patch ---"
+    .INCLUDE "includes/save_state_includes.s"
+.ENDS
+
+
+; Generated with patch-builder.py
