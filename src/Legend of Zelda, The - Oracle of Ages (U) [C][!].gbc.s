2b610863e020f2f8e820d68bd355796a78be57b6
Matt Currie <me@mattcurrie.com>
2021-08-13 08:49:22 +1200

    Add patches for Legend of Zelda - Oracle of Ages/Seasons

---
+++ src/Legend of Zelda, The - Oracle of Ages (U) [C][!].gbc.s
@@ -0,0 +1,42 @@
+; md5 c4639cc61c049e5a085526bb6cac03bb
+
+.INCLUDE "includes/init.s"
+.ROMBANKS 64
+.BACKGROUND "Legend of Zelda, The - Oracle of Ages (U) [C][!].gbc"
+.INCLUDE "includes/header.s"
+
+
+; config
+.DEFINE is_cgb 1
+.DEFINE current_rom_bank $ff97
+.DEFINE game_uses_save_ram 1
+.DEFINE uses_mbc5 1
+
+
+; joypad
+.DEFINE joypad $c481
+.DEFINE joypad_2 $c480
+.DEFINE joypad_3 $c482
+
+.BANK $0000 SLOT 0
+.ORG $3ef8
+.SECTION "relocated read from joypad" SIZE $003b OVERWRITE
+    .INCLUDE "includes/relocated_read_from_joypad.s"
+.ENDS
+
+.ORG $0290
+.SECTION "joypad read" SIZE 3 OVERWRITE
+    call relocated_read_from_joypad
+.ENDS
+
+
+; save/load state
+.BANK $0006 SLOT 1
+.ORG $3a32
+.SECTION "save/load state" SIZE $0315 OVERWRITE
+    .DB "--- Save Patch ---"
+    .INCLUDE "includes/save_state_includes.s"
+.ENDS
+
+
+; Generated with patch-builder.py
