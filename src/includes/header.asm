91a21f534f3e47e4a0f2f5a2f5a1de8d557d3802
Matt Currie <me@mattcurrie.com>
2024-10-23 20:39:16 +1300

    Set cartridge type

--- src/includes/header.asm
+++ src/includes/header.asm
@@ -1,4 +1,13 @@
 ; header
+
+SECTION "cartridge type", ROM0[$0147]
+IF DEF(uses_mbc5)
+    DB CART_ROM_MBC5_RAM_BAT
+ELSE
+    DB CART_ROM_MBC1_RAM_BAT
+ENDC
+ENDSECTION
+
 SECTION "ram size", ROM0[$0149]
     DB RAMSIZE
 ENDSECTION
